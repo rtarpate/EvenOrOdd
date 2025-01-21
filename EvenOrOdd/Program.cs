05eba83c4e6cf14b7cd86abda2ed69abe9e6693a
Ashton Pate <arpate@amarillocollege.com>
2025-01-21 13:59:40 -0600

    final code

--- EvenOrOdd/Program.cs
+++ EvenOrOdd/Program.cs
@@ -13,9 +13,20 @@ namespace EvenOrOdd
             //get user to input an number
             Console.WriteLine("Input a number vaule");
             // collect the number 
-            double usernumber = double.Parse(Console.ReadLine());
-            //test number to see if even or odd
-            
+            int usernumber = Convert.ToInt32(Console.ReadLine());
+            //test number to see if even or odd by seeing if its divisible by 2
+            if (usernumber % 2 == object)
+            {
+                //print out result
+                Console.WriteLine(usernumber + " : is even number");
+            }
+            else 
+            {
+                //print out results
+                Console.WriteLine(usernumber + " : is odd number");
+            }
+
+            Console.ReadLine();
             
 
         }
