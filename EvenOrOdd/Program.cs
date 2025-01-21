ae3431c3f2e81a4eebf7b09c56a995c4d4b5b67a
Ashton Pate <arpate@amarillocollege.com>
2025-01-21 13:55:17 -0600

    Add project files.

---
+++ EvenOrOdd/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvenOrOdd
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //get user to input an number
+            Console.WriteLine("Input a number vaule");
+            // collect the number 
+            double usernumber = double.Parse(Console.ReadLine());
+            //test number to see if even or odd
+            
+            
+
+        }
+    }
+}
